09b697619773bf5a793678c54e2311673d4a9990
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2021-09-28 13:50:53 +0300

    ttytryurt

---
+++ src/ilkproje/Main.java
@@ -0,0 +1,15 @@
+package ilkproje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// sysout ctrl space
+		System.out.println("Merhaba Java");
+		System.out.print("Emrah");
+		System.out.print(" ");
+		System.out.println("Bayram");//println bayram yazdýktan sonra imleci alt satýra indirir
+		System.out.println("Muzaffer"+" "+"Övün");
+		System.out.println("Yaþ :"+38);
+	}
+}
